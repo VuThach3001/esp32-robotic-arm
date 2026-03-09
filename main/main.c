e522aa2d21f855b5adb7ee4e75c1eb062be10c7e
Nguyen Ba Vu Thach <thach30012@gmail.com>
2026-03-09 09:16:28 +0700

    update: Configure build workspace for esp32 successfully

---
+++ main/main.c
@@ -0,0 +1,25 @@
+/**
+ * @file       main.c
+ * @copyright  Copyright (C) 2025 NEO. All rights reserved.
+ * @license    This project is released under the NEO License.
+ * @version    1.0.0
+ * @date       2026-03
+ * @author     Thach Nguyen Ba Vu
+ * @brief      Main file
+ * @note       None
+ */
+/* Includes ----------------------------------------------------------- */
+#include "stdio.h"
+/* Private defines ---------------------------------------------------- */
+/* Private enumerate/structure ---------------------------------------- */
+/* Private macros ----------------------------------------------------- */
+/* Public variables --------------------------------------------------- */
+/* Private variables -------------------------------------------------- */
+/* Private function prototypes ---------------------------------------- */
+/* Function definitions ----------------------------------------------- */
+void app_main(void)
+{
+    // Initialize BSP and libraries
+    printf("Hello, World!\n");
+}
+/* End of file -------------------------------------------------------- */
