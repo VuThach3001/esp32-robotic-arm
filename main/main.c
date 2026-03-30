5805c6b64415f9b11d8c0119db0f3e7725ef0155
ThachVu <thach30012@gmail.com>
2026-03-30 15:35:17 +0700

    update: Finish update workspace for tracking files

--- main/main.c
+++ main/main.c
@@ -10,16 +10,24 @@
  */
 /* Includes ----------------------------------------------------------- */
 #include "stdio.h"
+#include "I2c.h"
 /* Private defines ---------------------------------------------------- */
 /* Private enumerate/structure ---------------------------------------- */
 /* Private macros ----------------------------------------------------- */
 /* Public variables --------------------------------------------------- */
+I2C_ConfigType i2c_config = {
+    .i2c_port = 0,
+    .sda_pin = 12,
+    .scl_pin = 13,
+};
 /* Private variables -------------------------------------------------- */
 /* Private function prototypes ---------------------------------------- */
 /* Function definitions ----------------------------------------------- */
 void app_main(void)
 {
     // Initialize BSP and libraries
+    I2C_Init(&i2c_config);
+    printf("No error!\n");
     printf("Hello, World!\n");
 }
 /* End of file -------------------------------------------------------- */
